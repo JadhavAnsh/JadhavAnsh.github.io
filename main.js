d6039025e92f5455bd5f2c3258f7d9b6cb094356
jadhavansh10 <jadhavansh10@gmail.com>
2024-12-07 07:31:26 +0000

    made responsive navBar, added pages, added icons of tech-stack

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// Add JS here
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+
+function toggleMenu() {
+    var navbar = document.querySelector('.navbar');
+    var navbarLinks = document.querySelector('.navbar-links');
+    navbar.classList.toggle('active'); // Toggles the hamburger-to-cross transformation
+    navbarLinks.classList.toggle('active'); // Toggles the visibility of the menu
+  }
+  
+  
